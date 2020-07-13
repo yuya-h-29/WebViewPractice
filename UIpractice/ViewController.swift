9c629b71d45d9def97e678f702b2a3fce9b388c7
yuya-h-29 <tzou11291996@gmail.com>
2020-07-14 07:10:35 +0900

    Initial Commit

---
+++ UIpractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIpractice
+//
+//  Created by Yuya Harada on 2020/07/14.
+//  Copyright © 2020 Yuya Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
